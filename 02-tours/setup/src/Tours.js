c758383fb6d6a87f326d67dfbea29f5ef7143c24
faazah <91751221+faazah@users.noreply.github.com>
2022-07-04 12:25:00 +0530

    tours display implemented
    use of useState and useEffetct hook was carried out in this project

--- 02-tours/setup/src/Tours.js
+++ 02-tours/setup/src/Tours.js
@@ -1,7 +1,20 @@
-import React from 'react';
-import Tour from './Tour';
-const Tours = () => {
-  return <h2>tours component</h2>;
+import React from "react";
+import Tour from "./Tour";
+const Tours = ({ tours, removeTour }) => {
+  // console.log(tours);
+  return (
+    <section>
+      <div className="title">
+        <h2>Our Tours</h2>
+        <div className="underline"></div>
+      </div>
+      <div>
+        {tours.map((item) => {
+          return <Tour key={item.id} {...item} removeTour={removeTour} />;
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default Tours;
